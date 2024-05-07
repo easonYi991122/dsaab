e43505e9cb7707ba68cc1aec159d578b7b69dcb3
easonYi991122 <enian991122@gmail.com>
2024-05-08 03:12:55 +0800

    1

--- final project/src/Main.java
+++ final project/src/Main.java
@@ -6,8 +6,6 @@ public class Main {
         // 正确的字符串声明和变量名称
         String testFilename = "file:///D:/study/dsaab/final%20project/ex1.jpg";
 
-        // 假设SeamCarver的构造函数接受图片路径，这里使用了testFilename变量
-        // 注意：这里的参数数量和类型仅为示例，您需要根据SeamCarver构造函数的实际定义来调整
         SeamCarver sc = new SeamCarver(testFilename, 800, 1500, "", "");
 
         // Save or display the result
